99340813fd8057bb9eecdb818f947e791e24ac91
Dzonixy <dzonixy@gmail.com>
2022-12-26 21:25:17 +0100

    test cancelation by delegate

--- tests/delegate-manager.ts
+++ tests/delegate-manager.ts
@@ -85,4 +85,75 @@ describe("delegate-manager", () => {
       assert.ok(`${error}`.includes("Account does not exist or has no data"));
     }
   });
+
+  it("Initialize, confirm, cancel by delegate", async () => {
+    const authority = Keypair.generate();
+    const delegate = Keypair.generate();
+
+    await connection.confirmTransaction(
+      await connection.requestAirdrop(authority.publicKey, LAMPORTS_PER_SOL)
+    );
+    await connection.confirmTransaction(
+      await connection.requestAirdrop(delegate.publicKey, LAMPORTS_PER_SOL)
+    );
+
+    const [delegation] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("authorize"),
+        authority.publicKey.toBuffer(),
+        delegate.publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+
+    await program.methods
+      .initializeDelegate()
+      .accounts({
+        authority: authority.publicKey,
+        delegator: delegate.publicKey,
+        delegation,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([authority])
+      .rpc();
+
+    assert.deepEqual(await program.account.delegation.fetch(delegation), {
+      authority: authority.publicKey,
+      delegator: delegate.publicKey,
+      authorised: false,
+    });
+
+    await program.methods
+      .confirmDelegate()
+      .accounts({
+        delegator: delegate.publicKey,
+        delegation,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([delegate])
+      .rpc();
+
+    assert.deepEqual(await program.account.delegation.fetch(delegation), {
+      authority: authority.publicKey,
+      delegator: delegate.publicKey,
+      authorised: true,
+    });
+
+    await program.methods
+      .cancelDelegate()
+      .accounts({ delegation, systemProgram: SystemProgram.programId })
+      .remainingAccounts([
+        { pubkey: authority.publicKey, isSigner: false, isWritable: true },
+        { pubkey: delegate.publicKey, isSigner: true, isWritable: false },
+      ])
+      .signers([delegate])
+      .rpc();
+
+    try {
+      await program.account.delegation.fetch(delegation);
+      assert(false);
+    } catch (error) {
+      assert.ok(`${error}`.includes("Account does not exist or has no data"));
+    }
+  });
 });
