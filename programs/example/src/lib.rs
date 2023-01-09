e46be526e531a1c9cbb7d31d371673576b64ae4a
Dzonixy <dzonixy@gmail.com>
2023-01-09 10:34:59 +0100

    adds an example program using delegate-manager

---
+++ programs/example/src/lib.rs
@@ -0,0 +1,59 @@
+use anchor_lang::prelude::*;
+
+declare_id!("972QDtrTG4KvzEVt6fvxNmXQpuRyFhnpcR4Ln9Y41w5a");
+
+#[program]
+pub mod example {
+    use delegate_manager::Representation;
+
+    use super::*;
+
+    pub fn increment_counter(ctx: Context<Count>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+
+        if counter.count == 0 {
+            counter.authority = ctx.accounts.payer.key();
+        } else {
+            if counter.authority != ctx.accounts.payer.key() {
+                let representation = Account::<Representation>::try_from(
+                    ctx.remaining_accounts
+                        .iter()
+                        .next()
+                        .expect("Missing Representation account"),
+                )
+                .expect("Wrong account passed as Representation account");
+                require_keys_eq!(representation.representative, ctx.accounts.payer.key());
+                require!(representation.authorised, CounterError::NotAuthorized);
+            }
+        }
+        counter.count += 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Count<'info> {
+    #[account(
+        init_if_needed,
+        seeds = [b"counter-state"],
+        bump,
+        payer = payer,
+        space = 8 + 4 + 32,
+    )]
+    pub counter: Box<Account<'info, Counter>>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Counter {
+    count: u32,
+    authority: Pubkey,
+}
+
+#[error_code]
+enum CounterError {
+    #[msg("The account provided has no authority!")]
+    NotAuthorized,
+}
