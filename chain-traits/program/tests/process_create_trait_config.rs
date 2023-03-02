ebaf3cdc7e92305c5f10e1e310e12bae34d819bc
goran9999 <gutovicgoran@gmail.com>
2023-03-02 15:57:26 +0100

    added test for creator-identified collection

--- chain-traits/program/tests/process_create_trait_config.rs
+++ chain-traits/program/tests/process_create_trait_config.rs
@@ -1,11 +1,11 @@
 // #![cfg(feature = "test-bpf")]
 mod utils;
 use crate::utils::{
-    create_and_verify_nft, create_nft_mint, fetch_nft_json, get_trait_config, store_trait_config,
-    UriMetadata,
+    create_and_verify_nft, create_nft_mint, get_trait_config, store_trait_config, UriMetadata,
 };
-use chain_traits::state::{find_trait_config_address, TraitConfig};
+use chain_traits::state::find_trait_config_address;
 use solana_program_test::tokio;
+use solana_sdk::signer::Signer;
 
 #[tokio::test]
 async fn process_create_trait_config_test_happy_path() {
@@ -38,5 +38,34 @@ async fn process_create_trait_config_test_happy_path() {
             .unwrap()
             .value,
         trait_map.get("Background").unwrap().get(0).unwrap().value
+    );
+}
+
+#[tokio::test]
+pub async fn create_config_non_collection() {
+    let context = &mut utils::program_test().start_with_context().await;
+
+    let nft_data = create_nft_mint(context).await;
+    let nft_metadata = create_and_verify_nft(context, &nft_data.0, None).await;
+
+    store_trait_config(context, &context.payer.pubkey().clone(), &nft_metadata)
+        .await
+        .unwrap();
+
+    let trait_config_address = find_trait_config_address(&context.payer.pubkey().clone());
+
+    let trait_config_account = get_trait_config(context, trait_config_address.0).await;
+
+    let trait_map = UriMetadata::map_traits();
+
+    assert_eq!(
+        trait_config_account
+            .available_traits
+            .get("Head")
+            .unwrap()
+            .get(0)
+            .unwrap()
+            .value,
+        trait_map.get("Head").unwrap().get(0).unwrap().value
     );
 }
