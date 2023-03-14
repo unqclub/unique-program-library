9273d423b35975aa55251833ceb46185068284e5
goran9999 <gutovicgoran@gmail.com>
2023-03-14 14:31:49 +0100

    rebuilt solita

--- chain-traits/program/src/lib.rs
+++ chain-traits/program/src/lib.rs
@@ -6,4 +6,4 @@ pub mod instruction;
 pub mod processor;
 pub mod state;
 pub mod utils;
-declare_id!("5njbwr7cEX9jcbEEaELNdJ1i58kdkpNMCs3K53w56oyA");
+declare_id!("EVYWVskBg3xFwL3Px5FvGY3iC3kR4n4Mo9AA8kpTk1JB");
